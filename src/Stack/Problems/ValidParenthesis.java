b1d4865d46a811169c7ab144bfbd2fa98273be7f
Fayaz139 <syedfayaz13941@gmail.com>
2026-01-15 21:07:32 +0530

    Stack and Queue Started

--- src/Stack/Problems/ValidParenthesis.java
+++ src/Stack/Problems/ValidParenthesis.java
@@ -6,30 +6,23 @@ import java.util.Stack;
 public class ValidParenthesis {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Stack<Character> ch = new Stack<>();
 
-        System.out.println("Enter the Parenthesis you want to validate");
-        String str = sc.next();
-        char[] c = str.toCharArray();
+        String s = sc.next();
+        char[] c = s.toCharArray();
         boolean valid = true;
 
-        Stack<Character> s = new Stack<>();
+        for (int i = 0; i <= c.length - 1; i++) {
+            if (c[i] == '{' || c[i] == '[' || c[i] == '(') {
+                ch.push(c[i]);
+            } else if (c[i] == '}' || c[i] == ']' || c[i] == ')') {
+                if (ch.isEmpty()) {
+                    valid = false;
+                    break;
+                }
 
-        for (int i = 0; i <= c.length-1; i++) {
-            if (c[i] == '(' || c[i] == '{' || c[i] == '[') {
-                s.push(c[i]);
-            }
-            else {
-                if (!s.isEmpty()) {
-                    if (s.peek() == '(' && c[i] == ')' || s.peek() == '[' && c[i] == ']' || s.peek() == '{' && c[i] == '}' ) {
-                        s.pop();
-                    }
-                    else if (c[i] == '(' || c[i] == '{' || c[i] == '[') {
-                        s.push(c[i]);
-                    }
-                    else {
-                        valid = false;
-                        break;
-                    }
+                if (c[i] == '}' && ch.peek() == '{' || c[i] == ']' && ch.peek() == '[' || c[i] == ')' && ch.peek() == '(') {
+                    ch.pop();
                 }
                 else {
                     valid = false;
@@ -38,11 +31,10 @@ public class ValidParenthesis {
             }
         }
 
-        if (s.isEmpty() && valid) {
-            System.out.println("True");
-        }
-        else {
-            System.out.println("False");
+        if (ch.isEmpty() && valid) {
+            System.out.println("Valid Braces Format");
+        } else {
+            System.out.println("Invalid Braces Format");
         }
     }
 }
